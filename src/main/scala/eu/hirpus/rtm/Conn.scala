efc05003148f370d4f166fe214779eaa890ed87d
gruggiero <giovanni.ruggiero@gmail.com>
2011-06-26 09:28:50 +0200

    Work in Progress

--- src/main/scala/eu/hirpus/rtm/Conn.scala
+++ src/main/scala/eu/hirpus/rtm/Conn.scala
@@ -1,20 +1,30 @@
 package eu.hirpus.rtm
 
 import dispatch._
+import scala.collection._
+import java.security.MessageDigest
 
 object Conn {
 
-import java.security.MessageDigest
+	val params = mutable.Map[String, String]()
+	params += ("method" -> "rtm.auth.getFrob")
+	params += ("api_key" -> "67a282bc0cb82f652d5191090e982dd2")
+	params += ("format" -> "json")
 
-	val url = "http://api.rememberthemilk.com/services/rest"
+	def encode(params: Map[String,String]) = {
+			params.s
+	}
+  // val url = "http://api.rememberthemilk.com/services/rest"
 
-//?method=rtm.auth.getFrob&api_key=7f05f918a68abc9185faa2fd6baf09b6
+  val url = "http://api.rememberthemilk.com/services/rest?method=rtm.auth.getFrob&api_key=67a282bc0cb82f652d5191090e982dd2"
 
-	def md5(s: String) = {
+  def md5(s: String) = {
     MessageDigest.getInstance("MD5").digest(s.getBytes).map("%02X" format _).mkString
-	}
+  }
   def main(args:Array[String]) = {
-		println (md5("Hello"))
-	}
+
+    println (md5("Hello"))
+			println("test")
+  }
 
 }
